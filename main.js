f9dfc45c497ab11923353b22dc57f74e088c8898
Susanne Arnetz Linder <susanne.arnetzlinder@medieinstitutet.se>
2023-02-24 09:45:17 +0100

    fixed a lot of things

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 document.querySelector('.kattenErik').innerHTML = `
 <div class="imgSlider">
-<div class="imgWrapper"><img name=slider src="https://www.shutterstock.com/image-photo/example-word-written-on-wooden-260nw-1765482248.jpg"/></div>
+<div class="imgWrapper"><img class="kitten-img" height="200" width="300" name=slider src="https://www.shutterstock.com/image-photo/example-word-written-on-wooden-260nw-1765482248.jpg"/></div>
 <button class="material-symbols-outlined arrow_left">
 arrow_left
 </button>
@@ -18,13 +18,27 @@ arrowLeft.addEventListener("click", imageSliderMinus);
 arrowRight.addEventListener("click", imageSliderPlus);
 
 let i = 0;
-let images = [];
-images[0] = "https://www.shutterstock.com/image-photo/example-word-written-on-wooden-260nw-1765482248.jpg";
-images[1] = "https://www.shutterstock.com/image-photo/example-word-written-on-wooden-260nw-1765482248.jpg";
-images[2] = "https://www.shutterstock.com/image-photo/example-word-written-on-wooden-260nw-1765482248.jpg";
+let images = [
+	{
+		img: "./img/sadcat1.jpg",
+		alt: "Cat with sad ears"
+	},
+	{
+		img: "./img/sadcat2.jpg",
+		alt: "Cat behind bars"
+	},
+	{
+		img: "./img/sadcat3.jpg",
+		alt: "Scary cat"
+	},
+	{
+		img: "./img/saddog.jpg",
+		alt: "Sad dog"
+	}
+];
 
 function imageSliderPlus(){
-	document.slider.src = images[i]; 
+	document.slider.src = images[i].img; 
 	if(i < images.length - 1){
 	  i++; 
 	} else { 
@@ -33,7 +47,7 @@ function imageSliderPlus(){
 }
 
 function imageSliderMinus(){
-	document.slider.src = images[i]; 
+	document.slider.src = images[i].img; 
 	if(i < 1){
 		i = images.length -1;
 	}else {
